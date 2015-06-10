ee9e0ae9918ea0f54036b38566c772fb77619020
Barret Schloerke <schloerke@gmail.com>
2015-06-09 21:32:34 -0400

    white space

--- inst/trelliscopeViewer/www/assets/custom/d3uniFilter.js
+++ inst/trelliscopeViewer/www/assets/custom/d3uniFilter.js
@@ -56,7 +56,7 @@ var d3univarYbrush = d3.svg.brush()
     .on("brush", d3univarYbrushFn);
 
 function d3univar(data, id) {
-   
+
    var plotType = data.plotType[0];
    var xlab = data.name[0];
    var ylab = "Frequency";
@@ -69,18 +69,18 @@ function d3univar(data, id) {
       xlab = "Frequency";
       yAxisOffset = -20;
    }
-   
+
    data = data.data;
-   
+
    $("#" + id).html("");
    $("#" + id).append("<div id=\"" + id + "Range\" class=\"filterRange\"></div>");
-   
+
    var svg = d3.select("#" + id).append("svg:svg")
        .attr("width", d3univarWidth + d3univarMargin.left + d3univarMargin.right)
        .attr("height", d3univarHeight + d3univarMargin.top + d3univarMargin.bottom)
      .append("g")
        .attr("transform", "translate(" + d3univarMargin.left + "," + d3univarMargin.top + ")");
-   
+
    // if there is a filter for this variable
    // we will use that to make sure the extent of the axes
    // reaches far enough
@@ -93,22 +93,22 @@ function d3univar(data, id) {
       var varName = activeVar.data("name");
       filter = filterData[varName];
    }
-   
+
    if(plotType == "hist") {
       var delta = data[1].xdat - data[0].xdat;
-      
+
       var xrange = d3.extent(data.map(function(d) { return d.xdat; }));
       xrange[0] = xrange[0] - (xrange[1] - xrange[0]) * 0.07;
       xrange[1] = xrange[1] + (xrange[1] - xrange[0]) * 0.07;
-      
+
       if(filter != undefined) {
          xrange[0] = Math.min(xrange[0], filter.from);
          xrange[1] = Math.max(xrange[1], filter.to);
       }
-      
+
       d3univarX.domain(xrange);
       d3univarY.domain([0, d3.max(data.map(function(d) { return d.ydat; }))]);
-      
+
       svg.selectAll(".bar")
          .data(data)
         .enter().append("rect")
@@ -117,31 +117,31 @@ function d3univar(data, id) {
          .attr("width", d3univarX(delta) - d3univarX(0) - 0.75)
          .attr("y", function(d) { return d3univarY(d.ydat); })
          .attr("height", function(d) { return d3univarHeight - d3univarY(d.ydat); });
-      
+
       svg.append("g")
          .attr("class", "x brush")
          .call(d3univarXbrush)
        .selectAll("rect")
          .attr("y", -6)
          .attr("height", d3univarHeight + 7);
-            
+
    } else if(plotType == "quant") {
       var xrange = d3.extent(data.map(function(d) { return d.x; }));
       xrange[0] = xrange[0] - (xrange[1] - xrange[0]) * 0.07;
       xrange[1] = xrange[1] + (xrange[1] - xrange[0]) * 0.07;
-      
+
       var yrange = d3.extent(data.map(function(d) { return d.y; }));
       yrange[0] = yrange[0] - (yrange[1] - yrange[0]) * 0.07;
       yrange[1] = yrange[1] + (yrange[1] - yrange[0]) * 0.07;
-      
+
       if(filter != undefined) {
          yrange[0] = Math.min(yrange[0], filter.from);
          yrange[1] = Math.max(yrange[1], filter.to);
       }
-      
+
       d3univarX.domain(xrange);
       d3univarY.domain(yrange);
-      
+
       svg.selectAll(".points")
          .data(data)
         .enter().append("svg:circle")
@@ -150,7 +150,7 @@ function d3univar(data, id) {
          .attr("r", function(d) { return 4; })
          .attr("cx", function(d) { return d3univarX(d.x); })
          .attr("cy", function(d) { return d3univarY(d.y); });
-      
+
       svg.append("g")
          .attr("class", "y brush")
          .call(d3univarYbrush)
@@ -160,23 +160,23 @@ function d3univar(data, id) {
    } else if(plotType == "bar") {
       // remove last dummy record
       data.pop();
-      
+
       var delta = data[1].ind - data[0].ind;
-      
+
       d3univarX.domain([0, d3.max(data.map(function(d) { return d.Freq; }))]);
       var yMax = d3.max(data.map(function(d) { return d.ind; }));
       d3univarY.domain([0, yMax]);
-      
+
       // console.log(data);
       // console.log(d3univarY.domain);
       // console.log(d3univarX.domain);
-      
+
       function hasClass(el, cls) {
          return($(el).attr("class").split(/\s/).indexOf(cls) >= 0)
       }
-      
+
       var isMouseDown = false, isSelected;
-            
+
       // svg.selectAll("*").remove();
       svg.selectAll(".bar")
          .data(data)
@@ -232,29 +232,29 @@ function d3univar(data, id) {
          .attr("y", function(d) { return d3univarY(yMax + 1 - d.ind - delta / 2) + 4; })
          .text(function(d) { return d.label; });
    }
-   
+
    svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + d3univarHeight + ")")
       .call(d3univarXaxis);
-   
+
    svg.append("text")
       .attr("class", "axis-label")
       .attr("text-anchor", "middle")
       .attr("transform", "translate(" + (d3univarWidth / 2) + "," + (d3univarHeight + 50) + ")")
       .text(xlab);
-   
+
    if(plotType != "bar") {
       svg.append("g")
          .attr("class", "y axis")
-         .call(d3univarYaxis);      
+         .call(d3univarYaxis);
    }
    svg.append("text")
       .attr("class", "axis-label")
       .attr("text-anchor", "middle")
       .attr("transform", "translate(" + yAxisOffset + "," + (d3univarHeight / 2) + ")rotate(-90)")
       .text(ylab);
-      
+
 }
 
 
@@ -262,20 +262,20 @@ function d3univar(data, id) {
 // // if the update button is clicked, change the values of the lower and upper inputs and trigger a change
 // $(document).on("click", "#univarFilterPlotSubmit", function(evt) {
 //    var curRange = d3univarXbrush.empty() ? "" : d3univarXbrush.extent();
-//    
+//
 //    if(curRange != "") {
 //       // clear out all columns
 //       // (this currently doesn't operate as a marginal filter)
-//       $(".columnFilterFrom,.columnFilterTo").each(function() { 
-//          $(this).val(""); $(this).trigger("change"); 
+//       $(".columnFilterFrom,.columnFilterTo").each(function() {
+//          $(this).val(""); $(this).trigger("change");
 //       })
-//       
+//
 //       // get the current column
 //       var el = $(evt.target);
 //       var column = el.attr("name");
-//       
+//
 //       console.log("column" + column);
-//       
+//
 //       // trigger a change on the appropriate range
 //       $("#lower_column_" + column).val(curRange[0]);
 //       $("#upper_column_" + column).val(curRange[1]);
@@ -283,8 +283,8 @@ function d3univar(data, id) {
 //       $("#upper_column_" + column).trigger("change");
 //       updated3footHist(column);
 //    }
-//    
+//
 //    d3univarXbrush.clear()
 // });
-// 
-// 
+//
+//
